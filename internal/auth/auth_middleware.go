ae2b23373a6e4501ab32e5152e240dfc1bd7e11b
sebuszqo <sekn.kn@gmail.com>
2024-08-25 14:09:15 +0200

    add JWTAccessTokenMiddleware used for protected endpoints

--- internal/auth/auth_middleware.go
+++ internal/auth/auth_middleware.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -12,18 +14,48 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
-// JWTAuthMiddleware validates JWT tokens
-func JWTAuthMiddleware(jwtManager JWTManagerInterface, unprotectedPaths []string) func(http.Handler) http.Handler {
+func (s *authService) JWTAccessTokenMiddleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Check if the request path is unprotected
-			for _, path := range unprotectedPaths {
-				if strings.HasPrefix(r.URL.Path, path) {
-					next.ServeHTTP(w, r)
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				writeJSONError(w, http.StatusUnauthorized, "Authorization header is required")
+				return
+			}
+
+			tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+			if tokenString == authHeader {
+				writeJSONError(w, http.StatusUnauthorized, "Invalid token format")
+				return
+			}
+
+			userID, err := s.jwtManager.ValidateAccessToken(tokenString)
+			if err != nil {
+				writeJSONError(w, http.StatusUnauthorized, "Invalid or expired token")
+				return
+			}
+
+			_, err = s.repo.GetUserByID(userID)
+			if err != nil {
+				if errors.Is(err, ErrUserNotFound) {
+					writeJSONError(w, http.StatusUnauthorized, ErrUserNotFound.Error())
+					return
+				} else {
+					fmt.Println("TU ZWRACAM?")
+					writeJSONError(w, http.StatusInternalServerError, ErrInternalError.Error())
 					return
 				}
 			}
 
+			ctx := context.WithValue(r.Context(), "userID", userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func (s *authService) JWTRefreshTokenMiddleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Validate the Authorization header
 			authHeader := r.Header.Get("Authorization")
 			if authHeader == "" {
@@ -38,10 +70,29 @@ func JWTAuthMiddleware(jwtManager JWTManagerInterface, unprotectedPaths []string
 				return
 			}
 
-			// Verify the JWT token
-			userID, err := jwtManager.VerifyJWT(tokenString)
+			userID, err := s.jwtManager.ExtractUserIDFromRefreshToken(tokenString)
 			if err != nil {
-				writeJSONError(w, http.StatusUnauthorized, "Invalid or expired token")
+				if errors.Is(err, ErrExpiredJWTToken) {
+					writeJSONError(w, http.StatusUnauthorized, ErrExpiredJWTToken.Error())
+					return
+				}
+				writeJSONError(w, http.StatusInternalServerError, ErrInternalError.Error())
+				return
+			}
+
+			existingUser, err := s.repo.GetUserByID(userID)
+			if err != nil {
+				if errors.Is(err, ErrUserNotFound) {
+					writeJSONError(w, http.StatusUnauthorized, ErrUserNotFound.Error())
+					return
+				} else {
+					writeJSONError(w, http.StatusInternalServerError, ErrInternalError.Error())
+					return
+				}
+			}
+			err = s.jwtManager.ValidateRefreshToken(tokenString, existingUser.HashToken)
+			if err != nil {
+				writeJSONError(w, http.StatusUnauthorized, ErrInvalidJWTRefreshToken.Error())
 				return
 			}
 
