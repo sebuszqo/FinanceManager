760c0dea7f590e62363b6addf1bb05169f870aa6
sebuszqo <sekn.kn@gmail.com>
2024-10-05 15:43:19 +0200

    database configuration for investment module

--- sql/01_create_all_tables.sql
+++ sql/01_create_all_tables.sql
@@ -30,3 +30,68 @@ CREATE TABLE if NOT EXISTS user_two_factor_secrets (
 );
 
 CREATE INDEX idx_user_two_factor_user_id ON user_two_factor_secrets (user_id);
+
+CREATE TABLE if NOT EXISTS portfolios (
+                        id UUID PRIMARY KEY,
+                        user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+                        name VARCHAR(100) NOT NULL,
+                        description TEXT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE if NOT EXISTS asset_types (
+                       id SERIAL PRIMARY KEY,
+                       type VARCHAR(50) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE IF NOT EXISTS assets (
+    id UUID PRIMARY KEY,
+    portfolio_id UUID REFERENCES portfolios(id) ON DELETE CASCADE,
+    name VARCHAR(100) NOT NULL,
+    ticker VARCHAR(10),
+    asset_type_id INT REFERENCES asset_types(id) ON DELETE RESTRICT,
+    coupon_rate NUMERIC(5, 2),                  -- Bonds: coupon rate
+    maturity_date DATE,                         -- Bonds: maturity_date
+    face_value NUMERIC(15, 2),                  -- Bonds: face_value
+    dividend_yield NUMERIC(5, 2),               -- Stocks: dividend
+    accumulation BOOLEAN,                       -- ETF: accumulating (true) distributing (false)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO asset_types (type) VALUES ('Stock');
+INSERT INTO asset_types (type) VALUES ('Bond');
+INSERT INTO asset_types (type) VALUES ('ETF');
+INSERT INTO asset_types (type) VALUES ('Cryptocurrency');
+INSERT INTO asset_types (type) VALUES ('Savings Accounts');
+INSERT INTO asset_types (type) VALUES ('Cash');
+
+CREATE TABLE if NOT EXISTS transaction_types (
+                        id SERIAL PRIMARY KEY,
+                        type VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id UUID PRIMARY KEY,
+    asset_id UUID REFERENCES assets(id) ON DELETE CASCADE,
+    transaction_type_id INT REFERENCES transaction_types(id),
+    quantity NUMERIC(15, 4) NOT NULL,
+    price NUMERIC(15, 2) NOT NULL,
+    transaction_date TIMESTAMP NOT NULL,
+    dividend_amount NUMERIC(15, 2),
+    coupon_amount NUMERIC(15, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO transaction_types (type) VALUES ('Buy');
+INSERT INTO transaction_types (type) VALUES ('Sell');
+INSERT INTO transaction_types (type) VALUES ('Dividend');
+INSERT INTO transaction_types (type) VALUES ('Coupon Payment');
+INSERT INTO transaction_types (type) VALUES ('Withdrawal');
+INSERT INTO transaction_types (type) VALUES ('Interest Payment');
+INSERT INTO transaction_types (type) VALUES ('Reinvestment');
+INSERT INTO transaction_types (type) VALUES ('Fee');
+
+
