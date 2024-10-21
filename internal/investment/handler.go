0da908b12e0e2ef2a89d3c2de8cec9dcf5020bfd
sebuszqo <sekn.kn@gmail.com>
2024-10-21 21:32:01 +0200

    add assets to investment handler

--- internal/investment/handler.go
+++ internal/investment/handler.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/google/uuid"
+	assets "github.com/sebuszqo/FinanceManager/internal/investment/asset"
 	portfolios "github.com/sebuszqo/FinanceManager/internal/investment/portfolio"
 	"net/http"
 	"time"
@@ -12,35 +13,49 @@ import (
 
 type InvestmentHandler struct {
 	portfolioService portfolios.Service
-	// assetService assets.Service
-	// transactionService transactions.Service
+	assetService     assets.Service
+	//transactionService transactions.Service
 	respondJSON  func(w http.ResponseWriter, status int, payload interface{})
 	respondError func(w http.ResponseWriter, status int, message string)
 }
 
 func NewInvestmentHandler(
 	portfolioService portfolios.Service,
+	assetsService assets.Service,
 	respondJSON func(w http.ResponseWriter, status int, payload interface{}),
 	respondError func(w http.ResponseWriter, status int, message string),
 ) *InvestmentHandler {
 	return &InvestmentHandler{
 		portfolioService: portfolioService,
+		assetService:     assetsService,
 		respondJSON:      respondJSON,
 		respondError:     respondError,
 	}
 }
 
-type CreatePortfolioRequest struct {
+type createPortfolioRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
-type PortfolioResponse struct {
+type portfolioResponse struct {
 	ID          string    `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type createAssetRequest struct {
+	Name        string `json:"name"`
+	Ticker      string `json:"ticker"`
+	AssetTypeID int    `json:"asset_type_id"`
+	// Add other optional fields (for bonds, stocks, ETFs, etc.)
+	CouponRate    *float64 `json:"coupon_rate,omitempty"`
+	MaturityDate  *string  `json:"maturity_date,omitempty"`
+	FaceValue     *float64 `json:"face_value,omitempty"`
+	DividendYield *float64 `json:"dividend_yield,omitempty"`
+	Accumulation  *bool    `json:"accumulation,omitempty"`
 }
 
 func (h *InvestmentHandler) getUserIDReq(w http.ResponseWriter, r *http.Request) string {
@@ -58,7 +73,7 @@ func (h *InvestmentHandler) CreatePortfolio(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	var req CreatePortfolioRequest
+	var req createPortfolioRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		h.respondError(w, http.StatusBadRequest, "Invalid request body")
 		return
@@ -82,7 +97,7 @@ func (h *InvestmentHandler) CreatePortfolio(w http.ResponseWriter, r *http.Reque
 	h.respondJSON(w, http.StatusCreated, map[string]interface{}{
 		"status":  "success",
 		"message": "Portfolio successfully created.",
-		"data": PortfolioResponse{
+		"data": portfolioResponse{
 			ID:          portfolio.ID.String(),
 			Name:        portfolio.Name,
 			Description: portfolio.Description,
@@ -117,7 +132,7 @@ func (h *InvestmentHandler) GetPortfolio(w http.ResponseWriter, r *http.Request)
 	h.respondJSON(w, http.StatusOK, map[string]interface{}{
 		"status":  "success",
 		"message": "Portfolio retrieved successfully.",
-		"data": PortfolioResponse{
+		"data": portfolioResponse{
 			ID:          portfolio.ID.String(),
 			Name:        portfolio.Name,
 			Description: portfolio.Description,
@@ -172,6 +187,9 @@ func (h *InvestmentHandler) UpdatePortfolio(w http.ResponseWriter, r *http.Reque
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
+	//if req.Description == nil {
+	//	fmt.Println("DESCIRPTION is not provided:")
+	//}
 	if (req.Name == nil && req.Description == nil) || (*req.Name == "" && *req.Description == "") {
 		h.respondError(w, http.StatusBadRequest, "At least one field (name or description) must be provided for update")
 		return
@@ -227,6 +245,207 @@ func (h *InvestmentHandler) DeletePortfolio(w http.ResponseWriter, r *http.Reque
 	h.respondJSON(w, http.StatusOK, map[string]interface{}{
 		"status":  "success",
 		"message": "Portfolio deleted successfully.",
+	})
+
+}
+
+func (h *InvestmentHandler) GetAssetTypes(w http.ResponseWriter, _ *http.Request) {
+	assetTypes := h.assetService.GetAssetTypes()
+	h.respondJSON(w, http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"data":   assetTypes,
+	})
+}
+
+func (h *InvestmentHandler) CreateAsset(w http.ResponseWriter, r *http.Request) {
+	userID := h.getUserIDReq(w, r)
+	if userID == "" {
+		return
+	}
+
+	// don't need to check for "" or something similar as it was checked by investment_middleware
+	portfolioID := r.Context().Value("portfolioID").(uuid.UUID)
+	owned, err := h.portfolioService.CheckPortfolioOwnership(r.Context(), portfolioID, userID)
+	if err != nil {
+		http.Error(w, "Failed to check portfolio ownership", http.StatusInternalServerError)
+		return
+	}
+	if !owned {
+		http.Error(w, "Unauthorized access to portfolio", http.StatusUnauthorized)
+		return
+	}
+	assetRequest := &createAssetRequest{}
+	if err := json.NewDecoder(r.Body).Decode(assetRequest); err != nil {
+		h.respondError(w, http.StatusBadRequest, "Invalid request payload")
+	}
+
+	if assetRequest.Name == "" || assetRequest.Ticker == "" || assetRequest.AssetTypeID == 0 {
+		h.respondError(w, http.StatusBadRequest, "Name, Ticker, and AssetTypeID are required fields")
+		return
+	}
+
+	// Validate AssetTypeID using cached asset types
+	isValid := h.assetService.IsValidAssetType(assetRequest.AssetTypeID)
+	if !isValid {
+		h.respondError(w, http.StatusBadRequest, "Invalid asset type")
+		return
+	}
+
+	// Check if the asset already exists in the portfolio
+	exists, err := h.assetService.DoesAssetExist(r.Context(), portfolioID, assetRequest.Name)
+	if err != nil {
+		h.respondError(w, http.StatusInternalServerError, "Failed to check if asset exists")
+		return
+	}
+	if exists {
+		h.respondError(w, http.StatusConflict, "Asset already exists in this portfolio")
+		return
+	}
+
+	asset := assets.Asset{
+		ID:          uuid.New(),
+		PortfolioID: portfolioID,
+		Name:        assetRequest.Name,
+		Ticker:      assetRequest.Ticker,
+	}
+
+	switch assetRequest.AssetTypeID {
+	case 1: // Stock
+		if assetRequest.DividendYield == nil {
+			h.respondError(w, http.StatusBadRequest, "DividendYield is required for stocks")
+			return
+		}
+		asset.DividendYield = *assetRequest.DividendYield
+		asset.CouponRate = 0       // not applicable for stocks
+		asset.MaturityDate = nil   // not applicable for stocks
+		asset.FaceValue = 0        // not applicable for stocks
+		asset.Accumulation = false // not applicable for stocks
+
+	case 2: // Bond
+		if assetRequest.CouponRate == nil || assetRequest.MaturityDate == nil || assetRequest.FaceValue == nil {
+			h.respondError(w, http.StatusBadRequest, "CouponRate, MaturityDate, and FaceValue are required for bonds")
+			return
+		}
+
+		maturityDate, err := time.Parse("2006-01-02", *assetRequest.MaturityDate) // Example format: YYYY-MM-DD
+		if err != nil {
+			h.respondError(w, http.StatusBadRequest, "Invalid MaturityDate format, expected YYYY-MM-DD")
+			return
+		}
+		asset.CouponRate = *assetRequest.CouponRate
+		asset.MaturityDate = &maturityDate
+		asset.FaceValue = *assetRequest.FaceValue
+		asset.DividendYield = 0    // not applicable for bonds
+		asset.Accumulation = false // not applicable for bonds
+
+	case 3: // ETF
+		if assetRequest.Accumulation == nil {
+			h.respondError(w, http.StatusBadRequest, "Accumulation status is required for ETFs")
+			return
+		}
+		asset.Accumulation = *assetRequest.Accumulation
+		asset.CouponRate = 0     // not applicable for ETFs
+		asset.MaturityDate = nil // not applicable for ETFs
+		asset.FaceValue = 0      // not applicable for ETFs
+		asset.DividendYield = 0  // not applicable for ETFs
+
+	case 4: // Cryptocurrency
+		// No specific fields required, so leave everything else as 0 or nil
+		asset.CouponRate = 0
+		asset.MaturityDate = nil
+		asset.FaceValue = 0
+		asset.DividendYield = 0
+		asset.Accumulation = false
+
+	case 5: // Savings Accounts
+		// No specific fields required, similar to Cryptocurrency
+		asset.CouponRate = 0
+		asset.MaturityDate = nil
+		asset.FaceValue = 0
+		asset.DividendYield = 0
+		asset.Accumulation = false
+
+	case 6: // Cash
+		// No specific fields required, similar to Cryptocurrency and Savings Accounts
+		asset.CouponRate = 0
+		asset.MaturityDate = nil
+		asset.FaceValue = 0
+		asset.DividendYield = 0
+		asset.Accumulation = false
+
+	default:
+		h.respondError(w, http.StatusBadRequest, "Unsupported asset type")
+		return
+	}
+
+	asset.AssetTypeID = assetRequest.AssetTypeID
+	if err := h.assetService.CreateAsset(r.Context(), &asset); err != nil {
+		h.respondError(w, http.StatusInternalServerError, "Failed to create asset")
+		return
+	}
+
+	h.respondJSON(w, http.StatusCreated, map[string]interface{}{
+		"status": "success",
+		"data":   asset,
+	})
+}
+
+func (h *InvestmentHandler) DeleteAsset(w http.ResponseWriter, r *http.Request) {
+	userID := h.getUserIDReq(w, r)
+	if userID == "" {
+		return
+	}
+
+	// Get portfolioID and assetID from context
+	portfolioID := r.Context().Value("portfolioID").(uuid.UUID)
+	assetID := r.Context().Value("assetID").(uuid.UUID)
+
+	// Single query to check if the asset belongs to the portfolio and if the user owns the portfolio
+	exists, err := h.assetService.DoesAssetBelongToUser(r.Context(), assetID, portfolioID, userID)
+	if err != nil {
+		h.respondError(w, http.StatusInternalServerError, "Failed to check asset and portfolio ownership")
+		return
+	}
+	if !exists {
+		h.respondError(w, http.StatusUnauthorized, "Unauthorized access or asset not found in portfolio")
+		return
+	}
+
+	// Proceed to delete the asset
+	if err := h.assetService.DeleteAsset(r.Context(), assetID); err != nil {
+		h.respondError(w, http.StatusInternalServerError, "Failed to delete asset")
+		return
+	}
+
+	h.respondJSON(w, http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"message": "Asset deleted successfully.",
+	})
+}
+
+func (h *InvestmentHandler) GetAllAssets(w http.ResponseWriter, r *http.Request) {
+	userID := h.getUserIDReq(w, r)
+	if userID == "" {
+		return
+	}
+
+	portfolioID := r.Context().Value("portfolioID").(uuid.UUID)
+	assetList, err := h.assetService.GetAllAssets(r.Context(), userID, portfolioID)
+
+	if err != nil {
+		fmt.Println("err", err.Error())
+		if errors.Is(err, assets.ErrAssetsNotFound) {
+			h.respondError(w, http.StatusNotFound, "No assets in this portfolio")
+			return
+		}
+		h.respondError(w, http.StatusInternalServerError, "Failed to retrieve assets list")
+		return
+	}
+
+	h.respondJSON(w, http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"message": "List of assets retrieved successfully.",
+		"data":    assetList,
 	})
 
 }
