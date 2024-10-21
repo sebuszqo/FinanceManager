5b2bd6691e83978c8245d5bc3b8a3c1d7aba6cb8
sebuszqo <sekn.kn@gmail.com>
2024-10-21 21:32:50 +0200

    change investment_middleware.go to accept more than one param argument

--- internal/investment/investment_middleware.go
+++ internal/investment/investment_middleware.go
@@ -5,23 +5,47 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"net/http"
+	"strings"
 )
 
-func (h *InvestmentHandler) ValidatePortfolioIDMiddleware(next http.Handler) http.Handler {
+func capitalizeFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToUpper(string(s[0])) + s[1:]
+}
+
+func (h *InvestmentHandler) ValidateInvestmentPathParamsMiddleware(next http.Handler, params ...string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		portfolioID := r.PathValue("portfolioID")
-		if portfolioID == "" {
-			fmt.Println("[Investment_Middleware] Portfolio ID is empty")
-			h.respondError(w, http.StatusBadRequest, "Portfolio ID is required")
-			return
+		for _, param := range params {
+			paramValue := r.PathValue(param)
+			if paramValue == "" {
+				fmt.Printf("[Investment_Middleware] %s is empty\n", param)
+				h.respondError(w, http.StatusBadRequest, capitalizeFirstLetter(fmt.Sprintf("%s is required", param)))
+				return
+			}
+
+			parsedUUID, err := uuid.Parse(paramValue)
+			if err != nil {
+				fmt.Printf("[Investment_Middleware] %s is invalid\n", param)
+				switch param {
+				case "portfolioID":
+					h.respondError(w, http.StatusNotFound, "Portfolio not found")
+					return
+				case "assetID":
+					h.respondError(w, http.StatusNotFound, "Asset not found")
+					return
+				case "transactionID":
+					h.respondError(w, http.StatusNotFound, "Transaction not found")
+					return
+				default:
+					http.Error(w, fmt.Sprintf("Invalid %s format", param), http.StatusBadRequest)
+				}
+
+			}
+			r = r.WithContext(context.WithValue(r.Context(), param, parsedUUID))
+
 		}
-		parsedPortfolioID, err := uuid.Parse(portfolioID)
-		if err != nil {
-			fmt.Println("[Investment_Middleware] Portfolio ID is invalid")
-			h.respondError(w, http.StatusNotFound, "Portfolio not found")
-			return
-		}
-		ctx := context.WithValue(r.Context(), "portfolioID", parsedPortfolioID)
-		next.ServeHTTP(w, r.WithContext(ctx))
+		next.ServeHTTP(w, r)
 	})
 }
