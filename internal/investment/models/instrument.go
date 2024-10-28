1bdf131ad7c5e3013efc7335fa8f598c314d2ab4
sebuszqo <sekn.kn@gmail.com>
2024-10-28 20:43:30 +0100

    add common instruments models

---
+++ internal/investment/models/instrument.go
@@ -0,0 +1,22 @@
+package models
+
+type Instrument struct {
+	ID            int
+	Symbol        string
+	Name          string
+	Exchange      string
+	ExchangeShort string
+	AssetTypeID   int
+	Price         float64
+	Currency      string
+}
+
+type InstrumentDTO struct {
+	Symbol        string  `json:"symbol"`
+	Name          string  `json:"name"`
+	Exchange      string  `json:"exchange"`
+	ExchangeShort string  `json:"exchangeShortName"`
+	Type          string  `json:"type"`
+	Price         float64 `json:"price"`
+	Currency      string  `json:"currency"`
+}
