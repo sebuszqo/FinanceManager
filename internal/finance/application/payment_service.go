a2d641225e3f113ededf35bc2ed3c5807dcbc97a
sebuszqo <sekn.kn@gmail.com>
2024-11-13 17:15:39 +0100

    implement payment service methods

--- internal/finance/application/payment_service.go
+++ internal/finance/application/payment_service.go
@@ -10,11 +10,27 @@ func NewPaymentService(repo domain.PaymentRepository) *PaymentService {
 	return &PaymentService{repo: repo}
 }
 
-func (s *PaymentService) ListPaymentMethods() ([]domain.PaymentMethod, error) {
-	methods, err := s.repo.FindAllPaymentMethods()
+func (s *PaymentService) GetAllPaymentMethods() ([]domain.PaymentMethod, error) {
+	methods, err := s.repo.GetAllPaymentMethods()
 	if err != nil {
 		return nil, err
 	}
 
+	if methods == nil {
+		return []domain.PaymentMethod{}, nil
+	}
+
 	return methods, nil
+}
+
+func (s *PaymentService) GetUserPaymentSources(userID string) ([]domain.PaymentSource, error) {
+	return s.repo.GetUserPaymentSources(userID)
+}
+
+func (s *PaymentService) DoesPaymentMethodExistByID(methodID int) (bool, error) {
+	return s.repo.PaymentMethodExists(methodID)
+}
+
+func (s *PaymentService) DoesUserPaymentSourceExistByID(sourceID int, userID string) (bool, error) {
+	return s.repo.UserPaymentSourceExists(sourceID, userID)
 }
